1bc144c86a1b45f3cf3c132051de4d313f9ddbd3
Hernan Meydac Jean <hernanmj@live.com.ar>
2013-10-26 11:01:48 -0300

    Updated Web.config target

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://deepsky.apphb.com',
         mail: {},
         database: {
             client: 'sqlite3',
